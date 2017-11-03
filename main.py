50b20784a3043dc0cb8a637e3b9d9af9e2289c2a
Katadoka <katadoka@gmail.com>
2017-11-03 22:38:44 +0200

    separation of sentences with a dot

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 with open('in.txt', 'r') as f_in:
 	input_text = f_in.read()
 sentences = input_text.split('.')
-output_text = ""
+output_text = ''
+point = '.'
 for index in range(0, 4):
 	sentence = sentences[index]
 	words = sentence.split(' ') 
 	words.reverse()
 	sentence = ' '.join(words)	
-	output_text = output_text + sentence
-	print(sentence)	
+	output_text = output_text + sentence + point
+	print(sentence + point)	
 with open('out.txt', 'w') as f_out:
-	f_out.write(output_text)
+	f_out.write(output_text)	
