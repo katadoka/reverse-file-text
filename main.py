072783d96d364be84768910d825281e4ab65390b
Katadoka <katadoka@gmail.com>
2017-11-04 20:32:02 +0200

    #6 remove unnessesary tabs

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 with open('in.txt', 'r') as f_in:
 	input_text = f_in.read()
-sentences = input_text.split('.')
+sentences = input_text.split('. ')
 output_text = ''
-point = '.'
+point = '. '
 for index in range(0, 4):
 	sentence = sentences[index]
-	words = sentence.split(' ') 
+	words = sentence.split(' ')
 	words.reverse()
-	sentence = ' '.join(words)	
+	sentence = ' '.join(words)
 	output_text = output_text + sentence + point
-	print(sentence + point)	
+	print(sentence + point)
 with open('out.txt', 'w') as f_out:
-	f_out.write(output_text)	
+	f_out.write(output_text)
